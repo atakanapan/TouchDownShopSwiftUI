d1a19fd6fd6b586fd942835633ee067a4fb6bd61
atakanapan <atakanapan12@gmail.com>
2023-01-23 14:47:22 +0300

    Initial Commit

---
+++ TouchdownShop/TouchdownShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownShopApp.swift
+//  TouchdownShop
+//
+//  Created by Atakan Apan on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
